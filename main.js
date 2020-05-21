b222cd45b62c77e0e5863a8e040b838edd9f3b8a
PatrykK01 <42960742+PatrykK01@users.noreply.github.com>
2020-05-21 21:30:41 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+console.log("funkcja zmiana sprawia że po naciśnięciu postaci na ekranie tytułowym na wysokości diva top pojawia się królik z stale włączoną animacją skoku");
+console.log("funkcja clickMe sprawia ze po naciśnięciu guzika zmienia się tło koloru diva top na czerwony chyba że jest już czerwony, to na zielony");
 const zmiana = {
      pos : document.getElementById("pos"),
      krolik : document.getElementById("krolik"),
@@ -11,8 +13,33 @@ const zmiana = {
         'click',
         this.skok.bind(this),   
     );
-}
+},
     
 };
 
+const clickMe = {
+    btn : document.querySelector(".btn"),
+    top : document.querySelector(".top"),
+    
+    tlo:function(){
+        if(this.top.style.background === "crimson"){
+            this.top.style.background = "green"
+        }else{
+        this.top.style.background = "crimson";
+        }
+    },
+
+  
+    init:function(){
+        this.btn.addEventListener(
+            'click',
+            this.tlo.bind(this),
+            );
+    },
+
+};
+  
+
 zmiana.init();
+
+clickMe.init();
